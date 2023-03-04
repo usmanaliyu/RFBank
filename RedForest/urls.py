3cbbf280b1e6fd2c2a724dabe5626ddd419d3609
usmanaliyu <osman.abbati@gmail.com>
2023-03-04 16:54:19 +0100

    urlsadded

---
+++ RedForest/urls.py
@@ -0,0 +1,68 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('private-banking/', views.privatebanking, name='privatebanking'),
+
+    path('investment-solutions/', views.investmensolutions, name='investmensolutions'),
+    path('advisory-solutions', views.advisorysolutions, name='advisorysolutions'),
+    path('discretionarymandates', views.discretionarymandates, name='discretionarymandates'),
+    path('alternative-and-private-market-investment', views.alternativeandprivatemarketinvestment, name='alternativeandprivatemarketinvestment'),
+    path('advanced-investment-solutions', views.advancedinvestmentsolutions, name='advancedinvestmentsolutions'),
+
+    path('wealth-services', views.wealthservices, name='wealthservices'),
+    path('wealth-solutions', views.wealthsolutions, name='wealthsolutions'),
+    path('wealth-planning', views.wealthplanning, name='wealthplanning'),
+    path('fund-services', views.fundservices, name='fundservices'),
+    path('trust-services', views.trustservices, name='trustservices'),
+
+    path('credit-and-financing', views.creditandfinancing, name='creditandfinancing'),
+
+    path('banking-services', views.bankingservices, name='bankingservices'),
+
+    path('independent-asset-managers', views.independentassetmanagers, name='independentassetmanagers'),
+
+    path('ebanking', views.ebanking, name='ebanking'),
+    path('rfb-banking', views.rfbbanking, name='rfbbanking'),
+    path('rfb-access', views.rfbaccess, name='rfbaccess'),
+    path('security', views.security, name='security'),
+    path('documentation', views.documentation, name='documentation'),
+
+    path('asset-management', views.assetmanagement, name='assetmanagement'),
+    path('investment-process', views.investmentprocess, name='investmentprocess'),
+    path('resposible-investing', views.resposibleinvesting, name='resposibleinvesting'),
+
+    path('insights', views.insights, name='insights'),
+
+    path('about', views.about, name='about'),
+
+    path('our-philosophy', views.ourphilosophy, name='ourphilosophy'),
+
+    path('our-history', views.ourhistory, name='ourhistory'),
+
+    path('our-organization', views.ourorganization, name='ourorganization'),
+    path('global-business-committee', views.globalbusinesscommittee, name='globalbusinesscommittee'),
+    path('auditor', views.auditor, name='auditor'),
+    path('regulations', views.regulations, name='regulations'),
+
+    path('our-locations', views.ourlocations, name='ourlocations'),
+
+    path('our-sponsorships', views.oursponsorships, name='oursponsorships'),
+    path('sailing', views.sailing, name='sailing'),
+    path('jazz', views.jazz, name='jazz'),
+    path('arts-and-lifestyle', views.artsandlifestyle, name='artsandlifestyle'),
+    path('talent-development', views.talentdevelopment, name='talentdevelopment'),
+
+    path('careers', views.careers, name='careers'),
+
+    path('sustainability', views.sustainability, name='sustainability'),
+
+    path('investors', views.investors, name='investors'),
+
+    path('ratingsandinstruments', views.ratingsandinstruments, name='ratingsandinstruments'),
+    path('analyst-coverage', views.analystcoverage, name='analystcoverage'),
+    path('share-repurchase', views.sharerepurchase, name='sharerepurchase'),
+
+]
---
+++ RedForest/views.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render,get_object_or_404
+from django.contrib import messages
+
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+######## PRIVATE BANKING######
+def privatebanking(request):
+    return render(request,'private-banking.html')
+
+def investmensolutions(request):
+    return render (request,'investment-solutions.html')
+def advisorysolutions(request):
+    return render(request,'private-banking/investment-solutions/advisory-solutions.html')
+def discretionarymandates(request):
+    return render(request,'private-banking/investment-solutions/discretionary-mandate.html')
+def alternativeandprivatemarketinvestment(request):
+    return render(request,'private-banking/investment-solutions/alternative-and-privatr-market-investments.html')
+def advancedinvestmentsolutions(request):
+    return render(request,'private-banking/investment-solutions/advanced-investment-solutions.html')
+
+def wealthservices(request):
+    return render(request, 'private-banking/wealth-services.html')
+def wealthsolutions(request):
+    return render(request,'private-banking/wealth-services/wealth-solutions.html')
+def wealthplanning(request):
+    return render(request,'private-banking/wealth-services/wealth-planning.html')
+def fundservices(request):
+    return render(request,'private-banking/wealth-services/fund-services.html')
+def  trustservices(request):
+    return render(request, 'private-banking/wealth-services/trust-services.html')
+
+
+def creditandfinancing(request):
+    return render(request,'private-banking/credit-and-financing.html')
+
+def bankingservices(request):
+    return render(request,'private-banking/banking-services.html')
+
+def independentassetmanagers(request):
+    return render(request, 'private-banking/independent-asset-managers.html')
+
+def ebanking(request):
+    return render(request,'private-banking/ebanking.html')
+def rfbbanking(request):
+    return render(request,'private-banking/ebanking/rfb-mobile-banking.html')
+def rfbaccess(request):
+    return render(request,'private-banking/ebanking/rfb-access.html')
+def security(request):
+    return render(request,'private-banking/ebanking/security.html')
+def documentation(request):
+    return render(request,'private-banking/ebanking/documentation.html')
+
+######### ASSET MANAGEMENT #############
+
+def assetmanagement(request):
+    return render(request,'asset-management.html')
+def investmentprocess(request):
+    return render(request,'asset-management/investment-process.html')
+def resposibleinvesting(request):
+    return render(request,'asset-management/responsible-investing.html')
+
+######### INSIGHT ##########
+
+def insights(request):
+    return render(request,'insights.html')
+
+######### ABOUT ##########
+
+def about(request):
+    return render(request,'about.html')
+
+def ourphilosophy(request):
+    return render(request,'about/our-philosophy.html')
+
+def ourhistory(request):
+    return render(request,'about/our-history.html')
+
+def ourorganization(request):
+    return render(request,'about/our-organization.html')
+def globalbusinesscommittee(request):
+    return render(request,'about/our-organization/global-business-committee.html')
+def auditor(request):
+    return render(request,'about/our-organization/auditor.html')
+def regulations(request):
+    return render(request, 'about/our-organization/regulations.html')
+
+def ourlocations(request):
+    return render(request,'about/our-locations.html')
+
+def oursponsorships(request):
+    return render(request,'about/our-sponsorships.html')
+def sailing(request):
+    return render(request,'about/our-sponsorships/sailing.html')
+def jazz(request):
+    return render(request,'about/our-sponsorships/jazz.html')
+def artsandlifestyle(request):
+    return render(request,'about/our-sponsorships/arts-and-lifestyle.html')
+def talentdevelopment(request):
+    return render(request,'about/our-sponsorships/talent-development.html')
+
+def careers(request):
+    return render(request,'about/careers.html')
+
+def sustainability(request):
+    return render(request,'about/sustainability.html')
+
+############ IVESTORS ##########
+def investors(request):
+    return render(request,'investors.html')
+
+def ratingsandinstruments(request):
+    return render(request,'investors/rating-and-instruments.html')
+def analystcoverage(request):
+    return render(request,'investors/analyst-coverage.html')
+def sharerepurchase(request):
+    return render(request,'investors/share-repurchase.html')
+
+
+########### CONTACT ###############
+
+def contact(request):
+    return render(request,'contact.html')
